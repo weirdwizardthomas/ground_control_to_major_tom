1a24b2ac0003d1d4397f487f6b3a66aebb64d6c4
devpresso <tuomas.koristka@gmail.com>
2022-08-08 22:26:58 +0200

    Add start and end date url parameters

---
+++ neo_tracker/helpers/date_validator.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+DATETIME_FORMAT = '%Y-%m-%d:%H-%M'
+
+
+def validate_time_continuum(end_date, start_date):
+    if end_date and start_date > end_date:
+        raise ValueError('End date is before start date.')
+
+
+def get_start_date(request):
+    start_date = request.GET.get('start_date')
+    try:
+        start_date = datetime.strptime(start_date, DATETIME_FORMAT)
+        return start_date
+    except ValueError:
+        raise ValueError('Invalid start date format.')
+
+
+def get_end_date(request):
+    end_date = request.GET.get('end_date')
+    try:
+        if end_date:
+            return datetime.strptime(end_date, DATETIME_FORMAT)
+
+    except ValueError:
+        raise ValueError('Invalid end date format.')
--- neo_tracker/views.py
+++ neo_tracker/views.py
@@ -1,12 +1,25 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 from django_tables2 import RequestConfig
 
+from neo_tracker.helpers.date_validator import *
 from neo_tracker.helpers.neo_table import NeoTable
 from neo_tracker.services import near_earth_object_fetcher_service
 
 
+@require_http_methods(['GET'])
 def index(request):
-    table = NeoTable(near_earth_object_fetcher_service.get_objects())
+    try:
+        start_date = get_start_date(request)
+        end_date = get_end_date(request)
+        validate_time_continuum(end_date, start_date)
+
+    except ValueError as error:
+        messages.error(request, error)
+        return render(request, 'neo_tracker/index.html')
+
+    table = NeoTable(near_earth_object_fetcher_service.get_objects(start_date=start_date, end_date=end_date))
     RequestConfig(request).configure(table)
     return render(request, 'neo_tracker/index.html', context={
         'table': table
