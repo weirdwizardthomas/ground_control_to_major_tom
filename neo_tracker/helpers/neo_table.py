29721423705dc6f10ad3e48e13e62e1f8b96dd3b
devpresso <tuomas.koristka@gmail.com>
2022-08-08 21:44:13 +0200

    Add a sortable table of NEOs, by default sorted by miss distance.

--- neo_tracker/helpers/neo_table.py
+++ neo_tracker/helpers/neo_table.py
@@ -1,9 +1,19 @@
 import django_tables2 as tables
 
 
-# the object name, size estimate, time and distance of the closest encounter
 class NeoTable(tables.Table):
-    name = tables.Column()
-
     class Meta:
         template_name = "django_tables2/bootstrap.html"
+        order_by = 'miss_distance'
+
+    name = tables.Column()
+    close_approach_date = tables.Column()
+    miss_distance = tables.Column()
+    estimated_diameter_min = tables.Column(empty_values=())
+    estimated_diameter_max = tables.Column(empty_values=())
+
+    def render_estimated_diameter_min(self, record):
+        return record.estimated_diameter()['estimated_diameter_min']
+
+    def render_estimated_diameter_max(self, record):
+        return record.estimated_diameter()['estimated_diameter_max']
--- neo_tracker/views.py
+++ neo_tracker/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_tables2 import RequestConfig
 
 from neo_tracker.helpers.neo_table import NeoTable
 from neo_tracker.services import near_earth_object_fetcher_service
@@ -6,7 +7,7 @@ from neo_tracker.services import near_earth_object_fetcher_service
 
 def index(request):
     table = NeoTable(near_earth_object_fetcher_service.get_objects())
-
+    RequestConfig(request).configure(table)
     return render(request, 'neo_tracker/index.html', context={
         'table': table
     })
