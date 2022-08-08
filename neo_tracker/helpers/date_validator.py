3aa0008ee427439d07f6a676cb177eb1386f4a9b
devpresso <tuomas.koristka@gmail.com>
2022-08-09 00:09:02 +0200

    Add empty value treatment to start date to be current time.

--- neo_tracker/helpers/date_validator.py
+++ neo_tracker/helpers/date_validator.py
@@ -10,6 +10,10 @@ def validate_time_continuum(end_date, start_date):
 
 def get_start_date(request):
     start_date = request.GET.get('start_date')
+
+    if not start_date:
+        return datetime.now()
+
     try:
         start_date = datetime.strptime(start_date, DATETIME_FORMAT)
         return start_date
