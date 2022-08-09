690395afd81917d9bc8cbc1d8857d120ebbfe6bd
devpresso <tuomas.koristka@gmail.com>
2022-08-09 13:48:51 +0200

    Add simple NASA API error handling
    (cherry picked from commit e8a76d978a30446b74f8c4b6b0dc98f4e0bebe26)

--- neo_tracker/services/near_earth_object_fetcher_service.py
+++ neo_tracker/services/near_earth_object_fetcher_service.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import pandas as pd
 
 from django.conf import settings
+from django.contrib import messages
+from requests import HTTPError
 
 from neo_tracker.models import NearEarthObject
 
@@ -16,18 +18,14 @@ def flatten_list(data):
     return list(itertools.chain.from_iterable(data))
 
 
-def handle_status_code(status_code):
-    # todo
-    pass
-
-
 def get_objects(start_date=datetime.now(), end_date=None):
     params = {'start_date': start_date,
               'end_date': end_date,
               'api_key': settings.NASA_API_KEY}
 
     response = requests.get(URL, params=params)
-    handle_status_code(status_code=response.status_code)
+    response.raise_for_status()
+
     data = response.json()['near_earth_objects'].values()
 
     return [NearEarthObject.from_dictionary(x) for x in flatten_list(data)]
@@ -38,6 +36,6 @@ def sort_by_miss_distance(near_earth_object):
 
 
 def as_dataframe(start_date=datetime.now(), end_date=None):
-    neos = sorted(get_objects(start_date, end_date), key=sort_by_miss_distance)
+    neos = sorted(get_objects(end_date), key=sort_by_miss_distance)
     df = pd.DataFrame(neos)
     return df.to_html()
--- neo_tracker/views.py
+++ neo_tracker/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django_tables2 import RequestConfig
+from requests import HTTPError
 
 from neo_tracker.helpers.date_validator import *
 from neo_tracker.helpers.neo_table import NeoTable
@@ -14,12 +15,17 @@ def index(request):
         start_date = get_start_date(request)
         end_date = get_end_date(request)
         validate_time_continuum(end_date, start_date)
-
     except ValueError as error:
         messages.error(request, error)
         return render(request, 'neo_tracker/index.html')
 
-    table = NeoTable(near_earth_object_fetcher_service.get_objects(start_date=start_date, end_date=end_date))
+    try:
+        near_earth_objects = near_earth_object_fetcher_service.get_objects(start_date=start_date, end_date=end_date)
+    except HTTPError as error:
+        messages.error(request, error)
+        return render(request, 'neo_tracker/index.html')
+
+    table = NeoTable(near_earth_objects)
     RequestConfig(request).configure(table)
     return render(request, 'neo_tracker/index.html', context={
         'table': table
