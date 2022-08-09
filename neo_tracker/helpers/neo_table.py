286ca2df0b0681169e5f785f82034fd262bac472
devpresso <tuomas.koristka@gmail.com>
2022-08-09 16:30:32 +0200

    Rename NEO table class
    (cherry picked from commit d3581199f071d40793b2fc61e9c53d0dcdae2780)

--- neo_tracker/helpers/neo_table.py
+++ neo_tracker/helpers/neo_table.py
@@ -1,7 +1,7 @@
 import django_tables2 as tables
 
 
-class NeoTable(tables.Table):
+class NearEarthObjectTable(tables.Table):
     class Meta:
         template_name = "django_tables2/bootstrap.html"
         order_by = 'miss_distance'
